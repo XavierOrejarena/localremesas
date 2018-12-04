b5a9c68854752c4bfd77c2d01b0b7e02da6e0b08
Xavier Orejarena <XavierOrejarena@gmail.com>
2018-12-04 11:52:54 -0400

    test2

--- cargar_pagos_in.php
+++ cargar_pagos_in.php
@@ -2,7 +2,7 @@
 header( 'Content-type: application/json' );
 include "connect.php";
 // $sql = "SELECT * FROM pagos_in WHERE estado = 'PENDIENTE'";
-$sql = "SELECT * FROM pagos_in WHERE estado <> 'PAGADO'";
+$sql = "SELECT * FROM pagos_in WHERE estado = 'PENDIENTE'";
 $result = mysqli_query($link, $sql);
 
 while ($row = mysqli_fetch_array($result)) {
