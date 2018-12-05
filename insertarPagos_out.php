e0dc87ef504067b0de1db1d337d89bc81a586ef8
Xavier Orejarena <XavierOrejarena@gmail.com>
2018-12-05 15:24:55 -0400

    test2

--- insertarPagos_out.php
+++ insertarPagos_out.php
@@ -11,7 +11,7 @@ for ($i=0; $i < sizeof($_POST['id_cuenta']); $i++) {
 	$id_cuenta = $_POST['id_cuenta'][$i];
 	$monto = $_POST['monto'][$i];
 
-	$sql = "INSERT INTO pagos_out (id_usuario, id_pago_in, id_cuenta, monto, estado, reg_date) VALUES ('$id_usuario', '$id_pago_in', '$id_cuenta', '$monto', 'EN ESPERA', DATE_ADD(NOW(),INTERVAL 3 HOUR))";
+	$sql = "INSERT INTO pagos_out (id_usuario, id_pago_in, id_cuenta, monto, estado, reg_date) VALUES ('$id_usuario', '$id_pago_in', '$id_cuenta', '$monto', 'EN ESPERA', DATE_ADD(NOW(),INTERVAL 3 HOUR)))";
 	if(mysqli_query($link, $sql)) {
 		$res['mensajes'] = 'Pagos agregados existosamente';
 		$res['errores'] = false;
