4d95bc0de159ef4f9fd4904f52aa3c8a55e83d08
Xavier Orejarena <XavierOrejarena@gmail.com>
2018-12-05 12:35:06 -0400

    test2

--- reset_DB.php
+++ reset_DB.php
@@ -67,7 +67,7 @@ if (mysqli_connect_errno()) {
 	monto VARCHAR(64),
 	referencia VARCHAR(64),
 	estado VARCHAR(64),
-	reg_date TIMESTAMP NOT NULL DATE_ADD(NOW(), INTERVAL 3 HOUR)
+	reg_date TIMESTAMP NOT NULL
 	)";
 
 	if ($link->query($sql) === TRUE) {
