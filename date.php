490b6ff58e6854b78673126a65df9f1bd2a2354e
Xavier Orejarena <XavierOrejarena@gmail.com>
2018-12-04 20:54:27 -0400

    test2

--- date.php
+++ date.php
@@ -3,7 +3,7 @@
 include "connect.php";
 
 // $result = mysqli_query($link, "SELECT CURRENT_TIMESTAMP");
-$result = mysqli_query($link, "SELECT CURRENT_TIMESTAMP");
+$result = mysqli_query($link, "SELECT CURRENT_TIMESTAMP + INTERVAL 2 HOUR");
 
 while ($row = mysqli_fetch_array($result)) {
 	echo $row[0], " ";
