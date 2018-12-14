4cfd0ac6645c4e7a5d4587dcf60414a11d68f04c
Xavier Orejarena <XavierOrejarena@gmail.com>
2018-12-14 02:13:25 -0400

    test2

--- login.php
+++ login.php
@@ -5,16 +5,16 @@
 
   if (!empty($_POST)) {
     include 'connect.php';
-    $username = $_POST['username'];
+    $username = strtoupper($_POST['username']);
     $res = $username;
-    // if (password_verify($_POST['password'], mysqli_fetch_assoc(mysqli_query($link, "SELECT * FROM usuarios WHERE username = '$username'"))['password'])) {
-    //   $res = true;
-    //   $_SESSION['username'] = $username;
-    //   $_SESSION['time'] = time();
-    //   $_SESSION['tipo'] = mysqli_fetch_assoc(mysqli_query($link, "SELECT * FROM usuarios WHERE username = '$username'"))['tipo'];
-    // } else {
-    //   $res = false;
-    // }
+    if (password_verify($_POST['password'], mysqli_fetch_assoc(mysqli_query($link, "SELECT * FROM usuarios WHERE username = '$username'"))['password'])) {
+      $res = true;
+      $_SESSION['username'] = $username;
+      $_SESSION['time'] = time();
+      $_SESSION['tipo'] = mysqli_fetch_assoc(mysqli_query($link, "SELECT * FROM usuarios WHERE username = '$username'"))['tipo'];
+    } else {
+      $res = false;
+    }
 
     echo json_encode($res);
   }
