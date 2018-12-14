0acc302f8a57520ed9874d22b45fe4b6be9f146e
Xavier Orejarena <XavierOrejarena@gmail.com>
2018-12-14 02:11:38 -0400

    test2

--- login.js
+++ login.js
@@ -43,6 +43,7 @@ const app = new Vue({
 		},
 		login(e) {
 			axios.post('./login.php', new FormData(e.target)).then(response => {
+				console.log(response.data);
 				if (response['data']) {
 					window.location.href = './index.html';
 				} else {
