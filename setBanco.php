b25ca372bcf857f4644e4c6906e31a070807bf69
Xavier Orejarena <XavierOrejarena@gmail.com>
2018-12-15 13:04:59 -0400

    test2

--- setBanco.php
+++ setBanco.php
@@ -10,16 +10,21 @@ $res = false;
 if (mysqli_query($link, "UPDATE bancos SET saldo = bancos.saldo + '$monto' WHERE id = '$id_banco'")){
     $res = true;
 
-    if (mysqli_fetch_assoc(mysqli_query($link, "SELECT nombre FROM bancos WHERE id = '$id_banco'"))['nombre'] == 'BCP' && strlen($referencia) == 8) {
-        $referencia = substr($referencia, 2);
-    }
-
-    if($id = mysqli_fetch_assoc(mysqli_query($link, "SELECT id FROM pagos_in WHERE referencia = '$referencia' AND monto = '$monto' AND id_banco = '$id_banco'"))['id']) {
+    if (mysqli_fetch_assoc(mysqli_query($link, "SELECT nombre FROM bancos WHERE id = '$id_banco'"))['nombre'] == 'BCP') {
+        $referencia = substr($referencia, -6);
+        if($id = mysqli_fetch_assoc(mysqli_query($link, "SELECT id FROM pagos_in WHERE RIGHT(referencia,6) = '$referencia' AND monto = '$monto' AND id_banco = '$id_banco'"))['id']) {
+            $sql = "UPDATE pagos_in SET estado = 'APROBADA' WHERE id = '$id'";
+            mysqli_query($link, $sql);
+            $sql = "UPDATE pagos_out SET estado = 'PENDIENTE' WHERE id_pago_in = '$id'";
+            mysqli_query($link, $sql);
+        }
+    } elseif($id = mysqli_fetch_assoc(mysqli_query($link, "SELECT id FROM pagos_in WHERE referencia = '$referencia' AND monto = '$monto' AND id_banco = '$id_banco'"))['id']) {
         $sql = "UPDATE pagos_in SET estado = 'APROBADA' WHERE id = '$id'";
         mysqli_query($link, $sql);
         $sql = "UPDATE pagos_out SET estado = 'PENDIENTE' WHERE id_pago_in = '$id'";
         mysqli_query($link, $sql);
     }
+
 }
 
 echo json_encode($res);
