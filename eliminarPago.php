79bd8e9bd347845870bae11dcd0c4e4d04a9b8bb
Xavier Orejarena <XavierOrejarena@gmail.com>
2019-02-19 22:30:51 -0400

    test2

--- eliminarPago.php
+++ eliminarPago.php
@@ -5,8 +5,13 @@ include "connect.php";
 
 $id = $_POST['id'];
 
+$id_banco = mysqli_fetch_assoc(mysqli_query($link, "SELECT id_banco FROM pagos_in WHERE id = '$id'"))['id_banco'];
+$monto = mysqli_fetch_assoc(mysqli_query($link, "SELECT monto FROM pagos_in WHERE id = '$id'"))['monto'];
+
+
 $sql = "DELETE FROM pagos_in WHERE id = '$id'";
 $res = mysqli_query($link, $sql);
+mysqli_query($link, "UPDATE bancos SET saldo = saldo - '$monto' WHERE id = '$id_banco'");
 
-echo json_encode($res);
+echo json_encode($id);
 ?>
