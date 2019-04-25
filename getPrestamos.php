389d17bd11bc59e41fec80662d4e0259a8ac826a
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2019-04-24 21:05:18 -0400

    test

--- getPrestamos.php
+++ getPrestamos.php
@@ -3,7 +3,7 @@ header( 'Content-type: application/json' );
 include "connect.php";
 
 // $sql = "SELECT * FROM prestamos WHERE monto > 0";
-$sql = "SELECT prestamos.*, bancos.nombre, bancos.divisa FROM prestamos, bancos WHERE prestamos.id_banco = bancos.id AND prestamos.monto > 0";
+$sql = "SELECT prestamos.*, bancos.nombre, bancos.divisa FROM prestamos, bancos WHERE prestamos.id_banco = bancos.id AND prestamos.monto";
 $result = mysqli_query($link, $sql);
 
 while ($row = mysqli_fetch_assoc($result)) {
