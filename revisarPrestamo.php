96e80900f1400b5c44e8397e34a185afe4aed980
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2019-07-10 20:59:58 -0400

    Se corrigio un bug donde no verificaba los ultimos digitos del numero de referencia en BCP en admin - prestamos.

--- revisarPrestamo.php
+++ revisarPrestamo.php
@@ -6,9 +6,14 @@ $amount = $_POST['amount'];
 $referencia = $_POST['referencia'];
 $id_banco = $_POST['id_banco'];
 $id_usuario = $_POST['id_usuario'];
+$nombre_banco = mysqli_fetch_assoc(mysqli_query($link, "SELECT nombre FROM bancos WHERE id = '$id_banco'"))['nombre'];
 
 if (!empty($_POST['id_usuario'])) {
-    $result = mysqli_query($link, "SELECT id FROM pagos_in WHERE referencia = '$referencia' AND monto = '$amount' AND id_banco = '$id_banco' AND flag = 1");
+    if ($nombre_banco == 'BCP') {
+        $result = mysqli_query($link, "SELECT id FROM pagos_in WHERE RIGHT(referencia, 6) = RIGHT('$referencia', 6) AND monto = '$amount' AND id_banco = '$id_banco' AND flag = 1");
+    } else {
+        $result = mysqli_query($link, "SELECT id FROM pagos_in WHERE referencia = '$referencia' AND monto = '$amount' AND id_banco = '$id_banco' AND flag = 1");
+    }
     if ($result->num_rows == 1) {
         $id_pago_in = mysqli_fetch_assoc($result)['id'];
         mysqli_query($link, "UPDATE pagos_in SET flag = 9, id_usuario = '$id_usuario' WHERE id = '$id_pago_in'");
