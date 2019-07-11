eacb2bfca784142bfa17b07063034b4a86dbad42
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2019-07-11 19:29:09 -0400

    Se agrega un archivo para saber como se verian los nombres sin acentos.

--- test.php
+++ test.php
@@ -7,11 +7,15 @@ function Unaccent($string)
     return preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml|caron);~i', '$1', htmlentities($string, ENT_COMPAT, 'UTF-8'));
 }
 
-$sql = "SELECT nombre FROM cuentas";
+$sql = "SELECT id, nombre FROM cuentas";
 $result = mysqli_query($link, $sql);
 
 while ($row = mysqli_fetch_assoc($result)) {
+    
     $nombre = strtoupper(Unaccent($row['nombre']));
+    $id = $row['id'];
+    // mysqli_query($link, "UPDATE cuentas SET nombre = '$nombre' WHERE id = '$id'");
+
     echo "<p>$nombre<p>";
 }
 
