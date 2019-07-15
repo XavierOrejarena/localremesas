2de8016a13e68474ca34c4587ff242783d0c6484
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2019-07-15 14:31:51 -0400

    Se cambio el chat id del canal de telegram ya que se pago a publico

--- pagar_out.php
+++ pagar_out.php
@@ -67,8 +67,9 @@ if ($id = $_POST['id']) {
 
 				$token = '716396100:AAFbVh6W950S4goHt30TVUXW3cuKGdWQmKM';
 				$chat_id = '@PagosLR';
+				$chat_id = -1001265304659;
 				$text = 'http://localremesas.com/comprobantes_out/'.$id."_".$random.".jpg";
-				file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$text");
+				// file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$text");
 				$aux = true;
 				while($row = mysqli_fetch_array($result)){
 					if ($row['estado'] != 'PAGADO') {
