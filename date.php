6409f76cf26cb655411c66fedca61f56f72aa5e2
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2019-07-26 19:50:19 -0400

    test time zone.

--- date.php
+++ date.php
@@ -1,5 +1,6 @@
-<?php echo date("Y-m-d h:i:s"), "<br>";
-
+<?php
+date_default_timezone_set('America/La_Paz');
+echo date("Y-m-d h:i:s"), "<br>";
 include "connect.php";
 
 // $result = mysqli_query($link, "SELECT CURRENT_TIMESTAMP");
@@ -9,7 +10,5 @@ $result = mysqli_query($link, "SELECT CURRENT_TIMESTAMP");
 while ($row = mysqli_fetch_array($result)) {
 	echo $row[0], " ";
 }
-
-
-
+// phpinfo();
 // echo mysqli_query($link, "SELECT CURDATE()");
