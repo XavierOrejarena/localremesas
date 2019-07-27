84ab221d224da5fc39030ee01784d478edf5241f
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2019-07-26 20:10:58 -0400

    test time zone.

--- date.php
+++ date.php
@@ -1,14 +1,23 @@
 <?php
-// date_default_timezone_set('America/La_Paz');
-echo date("Y-m-d h:i:s"), "<br>";
+// date_default_timezone_set('America/Nome');
+// echo date("Y-m-d h:i:s"), "<br>";
 include "connect.php";
+
+
+$offset=-4*60*60; //converting 5 hours to seconds.  Hora en localhost
+$dateFormat="d-m-Y H:i";
+$timeNdate=gmdate($dateFormat, time()+$offset);
+
+echo $timeNdate;
 
 // $result = mysqli_query($link, "SELECT CURRENT_TIMESTAMP");
 // $result = mysqli_query($link, "SELECT CURRENT_TIMESTAMP + INTERVAL 3 HOUR");
 $result = mysqli_query($link, "SELECT CURRENT_TIMESTAMP");
 
 while ($row = mysqli_fetch_array($result)) {
-	echo $row[0], " ";
+	// echo $row[0], " ";
 }
+
+// echo date_default_timezone_get();
 // phpinfo();
 // echo mysqli_query($link, "SELECT CURDATE()");
