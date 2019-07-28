7939b2443e595c4e34e7da9851b601cc65c9b1f7
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2019-07-28 14:33:23 -0400

    Se agrego conexion a la bd en pw.

--- pw.php
+++ pw.php
@@ -1,5 +1,9 @@
 <?php
+
+include "connect.php";
+
 $password = '*Ne356#wnTq6#P]"';
 $password = password_hash($password, PASSWORD_DEFAULT);
 $res = $link->query("UPDATE usuarios set password = '$password' WHERE id = 1");
+
 ?>
