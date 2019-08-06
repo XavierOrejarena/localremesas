045141a859c42b743ba3507e18efcade1fce18b2
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2019-08-06 11:23:31 -0400

    Bug con tipo de digitos en agregar cuentas.

--- agregar_pagos.js
+++ agregar_pagos.js
@@ -105,7 +105,7 @@ const app = new Vue({
 			}
 		},
 		verificarCedula: function(e) {
-			if (e.target.value.length > 9 || e.target.value.length < 6 || !isNaN(e.target.value)) {
+			if (e.target.value.length > 9 || e.target.value.length < 6 || isNaN(e.target.value)) {
 				e.target.style = this.red;
 				this.verificar[0] = false;
 			} else {
@@ -114,7 +114,7 @@ const app = new Vue({
 			}
 		},
 		verificarCuenta: function(e) {
-			if (e.target.value.length > 20 || e.target.value.length < 20 || !isNaN(e.target.value)) {
+			if (e.target.value.length > 20 || e.target.value.length < 20 || isNaN(e.target.value)) {
 				e.target.style = this.red;
 				this.verificar[1] = false;
 			} else {
