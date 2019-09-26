733f2157b2a7874c1965069fe59182ca9c3259d2
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2019-09-26 11:44:34 -0400

    AudioWord.

--- bots/AudioWordBot.php
+++ bots/AudioWordBot.php
@@ -137,11 +137,10 @@ function processMessage($message) {
   if (isset($message['text'])) {
     $word = $message['text'];
     $URL = file_get_contents("https://dictionary.cambridge.org/es/diccionario/ingles/$word");
-    $URL = json_decode($URL, true);
-    apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => $URL));
     preg_match_all('/data-src-mp3="/', $URL, $matches, PREG_OFFSET_CAPTURE);
     preg_match_all('/.mp3"/', $URL, $matches2, PREG_OFFSET_CAPTURE);
     $text = substr($URL, $matches[0][0][1]+14, $matches2[0][0][1]-$matches[0][0][1]-10);
+    apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => $text));
 
     $ch = curl_init("https://dictionary.cambridge.org$text");
     curl_setopt($ch, CURLOPT_HEADER, 0);
